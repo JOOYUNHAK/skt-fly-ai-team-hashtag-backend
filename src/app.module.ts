44c0319aa69e7adc79c9034960c10238a8f50b42
JOOYUNHAK <wndbsgkr@naver.com>
2023-01-03 00:15:07 +0900

    add: mysql 연동, config module 추가
    mysql 연동과 mysql 관련 민감 정보는 config 파일로 분리 시켰기 때문에 config module도 추가

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,4 +1,6 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
+import databaseConfiguration from 'config/database.configuration';
 import { AuthModule } from './api/v1/auth/auth.module';
 import { LikeModule } from './api/v1/like/like.module';
 import { SearchModule } from './api/v1/search/search.module';
@@ -6,14 +8,20 @@ import { UserModule } from './api/v1/user/user.module';
 import { VideoModule } from './api/v1/video/video.module';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { DatabaseModule } from './database/database.module';
 
 @Module({
   imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+      load: [databaseConfiguration]
+    }),
     AuthModule,
     SearchModule,
     LikeModule,
     UserModule,
-    VideoModule
+    VideoModule,
+    DatabaseModule
   ],
   controllers: [AppController],
   providers: [AppService],
