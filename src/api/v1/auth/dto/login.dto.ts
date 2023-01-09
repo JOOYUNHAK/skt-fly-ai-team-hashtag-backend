b25dbf77f49bfd878c1e1286f1b31f7cfabc8347
JOOYUNHAK <wndbsgkr@naver.com>
2023-01-09 23:52:27 +0900

    move: login dto files user폴더 밑으로 이동

--- src/api/v1/auth/dto/login.dto.ts
+++
@@ -1,6 +0,0 @@
-import { ApiProperty, PickType } from '@nestjs/swagger';
-import { User } from '../../user/entity/user.entity';
-
-export class LoginDto extends PickType(User, [
-    'phoneNumber', 'nickName'
-]) {}
