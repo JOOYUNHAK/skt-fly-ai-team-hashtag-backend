0c4b8e32cc25fadfe2677a3c20973017b8fba09a
JOOYUNHAK <wndbsgkr@naver.com>
2023-01-12 21:39:17 +0900

    add: video configuration to app.module
    video config 파일 configmodule load에 추가

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,18 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
+import videoConfiguration from 'config/video.configuration';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { VideoModule } from './video/video.module';
 
 @Module({
-  imports: [VideoModule],
+  imports: [
+    VideoModule,
+    ConfigModule.forRoot({
+      isGlobal: true,
+      load: [videoConfiguration]
+    })
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
