67d142f520f0c3b560d8883cda55a3e00ea31038
JOOYUNHAK <wndbsgkr@naver.com>
2023-01-15 23:28:01 +0900

    chore: change database mysql to mongoDB
    몽고db를 이용함에 따른 mysql entity 파일들 삭제

--- src/video/entity/tag.entity.ts
+++
@@ -1,9 +0,0 @@
-import { Column, Entity, PrimaryColumn } from "typeorm";
-
-@Entity('tag')
-export class Tag {
-    @PrimaryColumn()
-    id: number;
-    @Column({ type: 'varchar', length: 16, nullable: false })
-    tag: string;
-}
