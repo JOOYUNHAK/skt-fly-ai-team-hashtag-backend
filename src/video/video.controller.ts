539fd10a64a2026effaf4fc80b134620c0279592
JOOYUNHAK <wndbsgkr@naver.com>
2023-01-17 00:32:48 +0900

    add(video): upload controller for video api
    동영상 전달 받았을 때 저장하는 controller 추가

--- src/video/video.controller.ts
+++ src/video/video.controller.ts
@@ -1,9 +1,37 @@
-import { Body, Controller, Post } from "@nestjs/common";
+import { Controller, Post, UploadedFiles, UseInterceptors, ParseFilePipeBuilder, HttpStatus, Get, Body } from "@nestjs/common";
+import { CommandBus } from "@nestjs/cqrs";
+import { FileFieldsInterceptor } from "@nestjs/platform-express";
+import { storage } from "../multer/multer-option-storage";
+import { UploadFilesCommand } from "./command/upload-files.command";
+import { UploadFilesDto } from "./dto/uploadFiles.dto";
 
 @Controller('video')
-export class VideoController{
+export class VideoController {
+    constructor(
+        private readonly commandBus: CommandBus
+    ) {}
     @Post()
-    async upload(@Body() data: any) {
-        
+    @UseInterceptors(FileFieldsInterceptor([
+        { name: 'video', maxCount: 1 },
+        { name: 'image', maxCount: 1 }
+    ], { storage }))
+    async upload(
+        @UploadedFiles() files: Express.Multer.File[], 
+        @Body() uploadFilesDto: UploadFilesDto
+        ) {
+        const { video, image } = JSON.parse( JSON.stringify(files) );
+        const { userId: owner } = uploadFilesDto;
+        await this.commandBus.execute(new UploadFilesCommand(owner, video[0].path, image[0].path))
     }
+
+    @Get()
+    async streamingVideo() {
+
+    }
+
+    @Get()
+    async getVideoList() {
+
+    }
+
 }
