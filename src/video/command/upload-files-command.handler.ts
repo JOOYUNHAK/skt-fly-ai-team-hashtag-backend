21f908eee963922e451eace0e953fa564598f0c2
JOOYUNHAK <wndbsgkr@naver.com>
2023-01-17 23:50:13 +0900

    refactor: upload file handler to native query
    mongoose를 삭제하고 native query를 사용함에 따른 upload file handler 변경

--- src/video/command/upload-files-command.handler.ts
+++ src/video/command/upload-files-command.handler.ts
@@ -1,17 +1,16 @@
+import { Inject } from "@nestjs/common";
 import { ICommandHandler, CommandHandler } from "@nestjs/cqrs";
-import { InjectModel } from "@nestjs/mongoose";
-import { Model } from "mongoose";
+import { Db } from 'mongodb';
 import { UploadFilesCommand } from "./upload-files.command";
 
 @CommandHandler(UploadFilesCommand)
 export class UploadFilesCommandHandler implements ICommandHandler<UploadFilesCommand> {
     constructor(
-        @InjectModel('videos')
-        private videoModel: Model<'videos'>
+        @Inject('MONGO_CONNECTION')
+        private db: Db
     ) {}
 
     async execute(command: UploadFilesCommand) {
-        const newVideo = new this.videoModel(command);
-        await newVideo.save();
+        await this.db.collection('videos').insertOne(command);
     }
 }
