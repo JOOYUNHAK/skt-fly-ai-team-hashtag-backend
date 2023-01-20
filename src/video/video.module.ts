c6a8f7bd5851ea013437572c78d8e59972e3358d
JOOYUNHAK <wndbsgkr@naver.com>
2023-01-20 21:04:41 +0900

    add(video): add handlers, cqrs, database module
    데이터베이스와 cqrs모듈 video 모듈에 추가

--- src/video/video.module.ts
+++ src/video/video.module.ts
@@ -2,7 +2,9 @@ import { Module } from "@nestjs/common";
 import { CqrsModule } from "@nestjs/cqrs";
 import { DatabaseModule } from "src/database/database.module";
 import { UploadFilesCommandHandler } from "./command/upload-files-command.handler";
-import { GetStreamingPathQueryHandler } from "./query/get-streaming-path-query.handler";
+import { GetStreamingDataQueryHandler } from "./query/get-streaming-data-query.handler.ts";
+import { GetVideoPathQueryHandler } from "./query/get-video-path-query.handler";
+import { GetVideoPathQuery } from "./query/get-video-path.query";
 import { VideoController } from "./video.controller";
 
 @Module({
@@ -13,7 +15,8 @@ import { VideoController } from "./video.controller";
     controllers: [VideoController],
     providers: [
         UploadFilesCommandHandler,
-        GetStreamingPathQueryHandler
+        GetVideoPathQueryHandler,
+        GetStreamingDataQueryHandler
     ]
 })
 export class VideoModule {}
