f1faf87c439941e4d763eda4d00c5aca260c04dc
JOOYUNHAK <wndbsgkr@naver.com>
2023-01-25 00:10:38 +0900

    add(video): interface for typescript return type
    collection에 document지정

--- src/video/query/get-video-path-query.handler.ts
+++ src/video/query/get-video-path-query.handler.ts
@@ -1,6 +1,7 @@
 import { Inject } from "@nestjs/common";
 import { IQueryHandler, QueryHandler } from "@nestjs/cqrs";
-import { Db, ObjectId } from "mongodb";
+import { Db, Document, ObjectId, WithId } from "mongodb";
+import { VideoPath } from "../interface/video.interface";
 import { GetVideoPathQuery } from "./get-video-path.query";
 
 @QueryHandler(GetVideoPathQuery)
@@ -10,9 +11,9 @@ export class GetVideoPathQueryHandler implements IQueryHandler<GetVideoPathQuery
         private readonly db: Db
     ) { }
 
-    async execute(query: GetVideoPathQuery): Promise<any> {
+    async execute(query: GetVideoPathQuery): Promise<WithId<Document>> {
         return await this.db
-            .collection('videos')
+            .collection<VideoPath>('videos')
             .findOne(
                 { _id: new ObjectId(query.videoId) },
                 { projection: { _id: 0, videoPath: 1 } }
