4284da91c3f08507fbbd0550374610726bc00f96
JOOYUNHAK <wndbsgkr@naver.com>
2023-01-30 19:41:13 +0900

    add: mongo module, search module, config module

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,18 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config/dist';
+import databaseConfiguration from 'config/database.configuration';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { SearchModule } from './search/search.module';
 
 @Module({
-  imports: [],
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+      load: [databaseConfiguration]
+    }),
+    SearchModule
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
