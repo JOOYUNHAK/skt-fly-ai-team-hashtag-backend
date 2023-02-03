bbaa6ba2ed1b255a39c473f6d3d55c73524b370e
JOOYUNHAK <wndbsgkr@naver.com>
2023-02-04 01:45:30 +0900

    remove: user service global prefix

--- src/main.ts
+++ src/main.ts
@@ -1,14 +1,13 @@
 import { NestFactory } from '@nestjs/core';
+import { MicroserviceOptions } from '@nestjs/microservices';
+import { Transport } from '@nestjs/microservices/enums';
 import { SwaggerModule } from '@nestjs/swagger';
+import { join } from 'path';
 import { CreateApiDocument } from './api/v1/swagger/create.document';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-   
-  // http://localhost:8080/api/v1
-   app.setGlobalPrefix('api/v1')
-
   const apiDocumentOptions = new CreateApiDocument().initializeOptions();
   const apiDocument = SwaggerModule.createDocument(app, apiDocumentOptions);
   SwaggerModule.setup('api/v1/docs', app, apiDocument);
