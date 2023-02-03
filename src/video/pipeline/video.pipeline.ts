2ffc85a2b2d7abc03c86c8d69694b5c2a7487cd9
JOOYUNHAK <wndbsgkr@naver.com>
2023-02-04 01:48:20 +0900

    add: pipeline for mongodb to query video

---
+++ src/video/pipeline/video.pipeline.ts
@@ -0,0 +1,51 @@
+import { ObjectId } from "mongodb";
+
+/* 최신 비디오 순 파이프라인 */
+export const GetRecentVideoListPipeLine = [
+    {
+        "$project": {
+            "_id": 1,
+            "nickName": 1,
+            "thumbNailPath": 1,
+            "title": 1,
+            "tags": 1,
+            "likeCount": 1,
+            "uploadedAt": {
+                "$divide": [
+                    {
+                        "$subtract": [
+                            new Date().getTime(),
+                            "$uploadedAt"
+                        ]
+                    },
+                    1000
+                ]
+            }
+        }
+    },
+    { "$sort": { "$uploadedAt": 1 }}
+];
+
+/* 인기 비디오 순 파이프라인 */
+export const GetHotVideoListPipeLine = [
+    { $sort: { likeCount: -1 } },
+    { $skip: 3 },
+    {
+        $project: {
+            _id: 1,
+            thumbNailPath: 1,
+            title: 1,
+            tags: 1,
+            likeCount: 1
+        }
+    }
+]
+
+/* 썸네일 이미지 get 파이프라인 */
+export const GetThumbNailPathPipeLine = (userId: string) => {
+    return [
+        { $match: { _id: new ObjectId(userId) } }, // 해당 user의 비디오만
+        { $sort: { uploadedAt: 1 } }, // 날짜 최신순
+        { $project: { _id: 1, thumbNailPath: 1 } }
+    ]
+}
