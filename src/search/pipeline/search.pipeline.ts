f8d08cfc28140da3521592bff3c991831c385189
JOOYUNHAK <wndbsgkr@naver.com>
2023-02-05 02:16:00 +0900

    add: searchVideoPipeLine Projection

--- src/search/pipeline/search.pipeline.ts
+++ src/search/pipeline/search.pipeline.ts
@@ -8,6 +8,25 @@ export const SearchVideoPipeline = (arg: string): Object[] => {
                     'path': ['tags', 'title'],
                     'synonyms': 'videoCollectionSynonyms'
                 }
+            },
+            '$project': {
+                '_id': 1,
+                'nickName': 1,
+                'thumbNailPath': 1,
+                'title': 1,
+                'tags': 1,
+                'likeCount': 1,
+                'uploadedAt': {
+                    '$divide': [
+                        {
+                            '$subtract': [
+                                new Date().getTime(),
+                                '$uploadedAt'
+                            ]
+                        },
+                        1000
+                    ]
+                }
             }
         }
     ];
