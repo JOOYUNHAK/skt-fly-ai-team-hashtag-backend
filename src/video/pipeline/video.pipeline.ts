286d8c713a20259e1343973023546caa310af7bf
JOOYUNHAK <wndbsgkr@naver.com>
2023-02-05 19:06:21 +0900

    add: pipeline in getVideoDetailQuery

--- src/video/pipeline/video.pipeline.ts
+++ src/video/pipeline/video.pipeline.ts
@@ -11,41 +11,57 @@ export const GetRecentVideoListPipeLine = [
             "tags": 1,
             "likeCount": 1,
             "uploadedAt": {
-                "$divide": [
-                    {
-                        "$subtract": [
-                            new Date().getTime(),
-                            "$uploadedAt"
-                        ]
-                    },
-                    1000
+                "$subtract": [
+                    Date.now(),
+                    "$uploadedAt"
                 ]
             }
         }
     },
-    { "$sort": { "$uploadedAt": 1 }}
+    { "$sort": { "uploadedAt": 1 } }
 ];
 
 /* 인기 비디오 순 파이프라인 */
 export const GetHotVideoListPipeLine = [
-    { $sort: { likeCount: -1 } },
-    { $skip: 3 },
+    { '$sort': { 'likeCount': -1 } },
+    { '$limit': 3 },
     {
-        $project: {
-            _id: 1,
-            thumbNailPath: 1,
-            title: 1,
-            tags: 1,
-            likeCount: 1
+        '$project': {
+            '_id': 1,
+            'thumbNailPath': 1,
+            'title': 1,
+            'tags': 1,
+            'likeCount': 1
         }
     }
 ]
+
+/* 비디오 상세조회 파이프라인 */
+export const GetVideoDetailPipeLine = (videoId: string) => {
+    return [
+        { '$match': { '_id': new ObjectId(videoId) }},
+        { '$project': {
+            '_id': 1,
+            'nickName': 1,
+            'title': 1,
+            'tags': 1,
+            'likeCount': 1,
+            'videoPath': 1,
+            'uploadedAt': {
+                '$subtract': [
+                    Date.now(),
+                    '$uploadedAt'
+                ]
+            }
+        }}
+    ]
+}
 
 /* 썸네일 이미지 get 파이프라인 */
 export const GetThumbNailPathPipeLine = (userId: string) => {
     return [
         { '$match': { 'owner': userId } }, // 해당 user의 비디오만
         { '$sort': { 'uploadedAt': 1 } }, // 날짜 최신순
-        { '$project': { _id: 1, thumbNailPath: 1 } }
+        { '$project': { '_id': 1, 'thumbNailPath': 1 } }
     ]
 }
--- src/video/query/get-video-detail-query.handler.ts
+++ src/video/query/get-video-detail-query.handler.ts
@@ -2,6 +2,7 @@ import { Inject } from "@nestjs/common";
 import { IQueryHandler, QueryHandler } from "@nestjs/cqrs";
 import { Db, Document, ObjectId, WithId } from "mongodb";
 import { VideDetailInfo } from "../interface/video.interface";
+import { GetVideoDetailPipeLine } from "../pipeline/video.pipeline";
 import { GetVideoDetailQuery } from "./get-video-detail.query";
 
 @QueryHandler(GetVideoDetailQuery)
@@ -11,20 +12,10 @@ export class GetVideoDetailQueryHandler implements IQueryHandler<GetVideoDetailQ
         private readonly db: Db
     ) { }
 
-    async execute({ videoId }: GetVideoDetailQuery): Promise<WithId<Document>> {
+    async execute({ videoId }: GetVideoDetailQuery): Promise<any> {
         return await this.db
-            .collection<VideDetailInfo>('video')
-            .findOne(
-                { _id: new ObjectId(videoId) },
-                { projection: { 
-                    _id: 1,
-                    nickName: 1,
-                    title: 1,
-                    tags: 1,
-                    likeCount: 1,
-                    uploadedAt: 1,
-                    videoPath: 1,
-                 } }
-            )
-    }
+            .collection('video')
+            .aggregate(GetVideoDetailPipeLine(videoId))
+            .toArray()
+        }
 }
