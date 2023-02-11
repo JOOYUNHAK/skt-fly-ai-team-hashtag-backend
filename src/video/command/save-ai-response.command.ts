0c0a79f3af3b8a7005231ffc6f7009e0c38b91a2
JOOYUNHAK <wndbsgkr@naver.com>
2023-02-11 19:26:53 +0900

    change: imagePath to thumbNailPath variable name
    변수명 변경

--- src/video/command/save-ai-response.command.ts
+++ src/video/command/save-ai-response.command.ts
@@ -3,7 +3,7 @@ import { ICommand } from "@nestjs/cqrs";
 export class SaveAiResponseCommand implements ICommand {
     constructor(
         public readonly userId: string,
-        public readonly videoImage: string,
+        public readonly thumbNailPath: string,
         public readonly videoPath: string,
         public readonly videoTag: string []
     ) {}
