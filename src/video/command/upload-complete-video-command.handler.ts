73e01891f4f551bfe72cf996d7092e81b77ff7ce
JOOYUNHAK <wndbsgkr@naver.com>
2023-02-14 01:19:19 +0900

    add: delete key in  tempVideoData object
    데이터베이스에 저장하기 위해 불필요한 필드 object에서 삭제

--- src/video/command/upload-complete-video-command.handler.ts
+++ src/video/command/upload-complete-video-command.handler.ts
@@ -1,7 +1,8 @@
 import { Inject } from "@nestjs/common";
-import { CommandHandler, ICommandHandler, QueryBus } from "@nestjs/cqrs";
+import { CommandHandler, EventBus, ICommandHandler, QueryBus } from "@nestjs/cqrs";
 import { Db } from "mongodb";
 import { RedisClientType } from "redis";
+import { UploadVideoCompleteEvent } from "../event/upload-video-complete.event";
 import { GetTempVideoDataQuery } from "../query/get-temp-video-data.query";
 import { UploadCompleteVideoCommand } from "./upload-complete-video.command";
 
@@ -12,17 +13,20 @@ export class UploadCompleteVideoCommandHandler implements ICommandHandler<Upload
         private readonly redis: RedisClientType,
         @Inject('MONGO_CONNECTION')
         private readonly db: Db,
-        private readonly queryBus: QueryBus
+        private readonly queryBus: QueryBus,
+        private readonly eventBus: EventBus
     ) {}
     async execute(command: UploadCompleteVideoCommand): Promise<any> {
         const { userId, title } = command;
         let tempVideoData = await this.queryBus.execute(new GetTempVideoDataQuery(userId));
-        // 완료된 동영상 저장
+        const { originVideoPath } = tempVideoData;
+        delete tempVideoData['originVideoPath']; //원본 경로 삭제
+        // 완료된 동영상 몽고 db에 저장
         await this.db
             .collection('video') 
             .insertOne({...tempVideoData, title, likeCount: 0, uploadedAt: Date.now()});
         // redis temp video data 삭제
         await this.redis.HDEL('process:video:list', `user:${userId}`);
-        
+        this.eventBus.publish(new UploadVideoCompleteEvent(originVideoPath));
     }
 }
