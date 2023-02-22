4a6a3b0139766f69827a2410d38ea9861c1be6db
JOOYUNHAK <wndbsgkr@naver.com>
2023-02-22 17:13:43 +0900

    change: import multers3 for esmoduleinterop true

--- src/multer/multer-options.factory.ts
+++ src/multer/multer-options.factory.ts
@@ -1,7 +1,7 @@
 import { S3Client } from "@aws-sdk/client-s3";
 import { ConfigService } from "@nestjs/config";
 import { MulterOptions } from "@nestjs/platform-express/multer/interfaces/multer-options.interface";
-import * as multerS3 from 'multer-s3';
+import multerS3 from 'multer-s3';
 import { basename, extname } from "path";
 
 export const multerOptionsFactory = (configService: ConfigService): MulterOptions => {
