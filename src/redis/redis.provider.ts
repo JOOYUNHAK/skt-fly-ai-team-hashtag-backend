a1519c32bfc2e81e47e6b22eba7f395f430e13c2
JOOYUNHAK <wndbsgkr@naver.com>
2023-02-26 12:20:58 +0000

    change: redis url to docker address

--- src/redis/redis.provider.ts
+++ src/redis/redis.provider.ts
@@ -5,7 +5,7 @@ export const redisProvider = [
         provide: 'REDIS_CLIENT',
         useFactory: async () => {
             const client = createClient({
-                url: 'redis://127.0.0.1:6379'
+                url: 'redis://172.18.10.11:6379'
             });
             await client.connect();
             return client;
