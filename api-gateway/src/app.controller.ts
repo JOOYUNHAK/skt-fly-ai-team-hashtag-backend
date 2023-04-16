de2ffd46a779608acc6510bbef0ad70c8f77f902
JOOYUNHAK <wndbsgkr@naver.com>
2023-04-17 02:05:06 +0900

    refactor: exception filter for aop

--- api-gateway/src/app.controller.ts
+++ api-gateway/src/app.controller.ts
@@ -1,5 +1,5 @@
 import { HttpService } from "@nestjs/axios";
-import { Body, Controller, Get, Param, Post, Put, Query, HttpException, HttpStatus, UseInterceptors, UseFilters, NotFoundException } from "@nestjs/common";
+import { Body, Controller, Get, Param, Post, Put, Query, HttpException, HttpStatus, UseInterceptors, UseFilters } from "@nestjs/common";
 import { ConfigService } from "@nestjs/config";
 import { AxiosResponse } from "axios";
 import { SaveCommentDto } from "./comment-service/dto/comment-service.dto";
@@ -9,20 +9,22 @@ import { SaveVideoPathDto } from "./video-service/dto/save-video-path.dto";
 import { SaveVideoTitleDto } from "./video-service/save-video-title.dto";
 import { map, mergeWith, Observable } from "rxjs";
 import { ResponseTransformInterceptor } from "./interceptor/transform.interceptor";
+import { AllHttpExceptionFilter } from "./filter/http-exception.filter";
 
 @Controller('api/v1')
 @UseInterceptors(ResponseTransformInterceptor)
+@UseFilters(AllHttpExceptionFilter)
 export class ApiGateway {
   constructor(
     private readonly httpService: HttpService,
     private readonly configService: ConfigService
   ) { }
 
-    private readonly USER_SERVICE: string = this.configService.get('docker.user_service'); 
-    private readonly LIKE_SERVICE: string = this.configService.get('docker.like_service');
-    private readonly SEARCH_SERVICE: string = this.configService.get('docker.search_service');
-    private readonly VIDEO_SERVICE: string = this.configService.get('docker.video_service');
-    private readonly COMMENT_SERVICE: string = this.configService.get('docker.comment_service');
+  private readonly USER_SERVICE: string = this.configService.get('docker.user_service');
+  private readonly LIKE_SERVICE: string = this.configService.get('docker.like_service');
+  private readonly SEARCH_SERVICE: string = this.configService.get('docker.search_service');
+  private readonly VIDEO_SERVICE: string = this.configService.get('docker.video_service');
+  private readonly COMMENT_SERVICE: string = this.configService.get('docker.comment_service');
 
   /*
    * Service: User/Auth 
@@ -31,34 +33,17 @@ export class ApiGateway {
   */
   @Get('user/feed/:id')
   getMyFeedRequest(@Param('id') id: string): Observable<AxiosResponse> {
-    try {
-      return this.httpService
-              .get(`${this.USER_SERVICE}/user/feed/${id}`)
-              .pipe( map(response => response.data) )
-    }
-    catch (err) {
-      throw new HttpException(
-        '죄송해요. 사용자서버에 문제가 생겨 복구중이에요... router -> user/feed',
-        HttpStatus.INTERNAL_SERVER_ERROR
-      );
-    }
+    return this.httpService
+      .get(`http://127.0.0.1:8080/user/feed/${id}`)
+      .pipe(map(response => response.data))
   }
 
   @Post('auth/login')
   loginRequest(@Body() loginRequestDto: LoginRequestDto): Observable<AxiosResponse> {
-    try {
-      const { phoneNumber, nickName } = loginRequestDto;
-      return this.httpService
-                .post(`${this.USER_SERVICE}/auth/login`, { phoneNumber, nickName })
-                .pipe( map(response => response.data) )
-    }
-    catch (err) {
-      console.log(err)
-      throw new HttpException(
-        '죄송해요 사용자서버에 문제가 생겨 복구중이에요... router -> auth/login',
-        HttpStatus.INTERNAL_SERVER_ERROR
-      )
-    }
+    const { phoneNumber, nickName } = loginRequestDto;
+    return this.httpService
+      .post(`${this.USER_SERVICE}/auth/login`, { phoneNumber, nickName })
+      .pipe(map(response => response.data))
   }
 
   /*
@@ -68,107 +53,53 @@ export class ApiGateway {
   */
   @Get('video/list')
   videoListRequest(): Observable<AxiosResponse> {
-    try {
-      return this.httpService
-              .get(`${this.VIDEO_SERVICE}/video/list`)
-              .pipe( map(response => response.data) )
-    }
-    catch (err) {
-      console.log(err)
-      throw new HttpException(
-        '죄송해요 비디오 서버에 문제가 생겨 복구중이에요... router -> video/list',
-        HttpStatus.INTERNAL_SERVER_ERROR
-      )
-    }
+    return this.httpService
+      .get(`${this.VIDEO_SERVICE}/video/list`)
+      .pipe(map(response => response.data))
   }
 
   @Get('video/detail/:videoId')
   videoDetailInfoRequest(@Param('videoId') videoId: string): Observable<AxiosResponse> {
-    try {
-      const [detail$, comments$] = [
-        this.httpService
-          .get(`${this.VIDEO_SERVICE}/video/detail/${videoId}`)
-          .pipe( map( response  => response.data )),
-        this.httpService
-          .get(`${this.COMMENT_SERVICE}/video/comment/${videoId}`)
-          .pipe( map( response => response.data ))
-      ];
-      return detail$.pipe( mergeWith( comments$ ));
-    }
-    catch (err) {
-      console.log(err)
-      throw new HttpException(
-        '죄송해요 비디오 서버에 문제가 생겨 복구중이에요... router -> video/detail',
-        HttpStatus.INTERNAL_SERVER_ERROR
-      )
-    }
+    const [detail$, comments$] = [
+      this.httpService
+        .get(`${this.VIDEO_SERVICE}/video/detail/${videoId}`)
+        .pipe(map(response => response.data)),
+      this.httpService
+        .get(`${this.COMMENT_SERVICE}/video/comment/${videoId}`)
+        .pipe(map(response => response.data))
+    ];
+    return detail$.pipe(mergeWith(comments$));
   }
 
   @Post('video/path')
-  saveThumbNailPath(@Body() saveVideoPathDto: SaveVideoPathDto):Observable<AxiosResponse>  {
-    try {
-      const { userId, nickName, videoPath } = saveVideoPathDto;
-      return this.httpService
-              .post(`${this.VIDEO_SERVICE}/video/path`, { userId, nickName, videoPath })
-              .pipe( map( response => response.data ))
-    }
-    catch (err) {
-      console.log(err)
-      throw new HttpException(
-        '죄송해요 비디오 서버에 문제가 생겨 복구중이에요... router -> video/path',
-        HttpStatus.INTERNAL_SERVER_ERROR
-      )
-    }
+  saveThumbNailPath(@Body() saveVideoPathDto: SaveVideoPathDto): Observable<AxiosResponse> {
+    const { userId, nickName, videoPath } = saveVideoPathDto;
+    return this.httpService
+      .post(`${this.VIDEO_SERVICE}/video/path`, { userId, nickName, videoPath })
+      .pipe(map(response => response.data))
   }
 
   @Post('video')
   saveVideoTitle(@Body() saveVideoTitleDto: SaveVideoTitleDto): Observable<AxiosResponse> {
-    try {
-      const { userId, title } = saveVideoTitleDto;
-      return this.httpService
-              .post(`${this.VIDEO_SERVICE}/video`, { userId, title })
-              .pipe( map( response => response.data ))
-    }
-    catch (err) {
-      console.log(err)
-      throw new HttpException(
-        '죄송해요 비디오 서버에 문제가 생겨 복구중이에요... router -> video',
-        HttpStatus.INTERNAL_SERVER_ERROR
-      )
-    }
+    const { userId, title } = saveVideoTitleDto;
+    return this.httpService
+      .post(`${this.VIDEO_SERVICE}/video`, { userId, title })
+      .pipe(map(response => response.data))
   }
 
   @Put('video')
-  notUploadVideo(@Body('userId') userId: string):Observable<AxiosResponse> {
-    try {
-      return this.httpService
-              .put(`${this.VIDEO_SERVICE}/video`, { userId })
-              .pipe( map( response => response.data ))
-    }
-    catch (err) {
-      console.log(err)
-      throw new HttpException(
-        '죄송해요 비디오 서버에 문제가 생겨 복구중이에요... router -> video, put...',
-        HttpStatus.INTERNAL_SERVER_ERROR
-      )
-    }
+  notUploadVideo(@Body('userId') userId: string): Observable<AxiosResponse> {
+    return this.httpService
+      .put(`${this.VIDEO_SERVICE}/video`, { userId })
+      .pipe(map(response => response.data))
   }
 
   @Post('test/video/path')
-  testVideoPathRouter(@Body() testDto: any):Observable<AxiosResponse> {
-    try {
-      const { userId, nickName, videoPath, category } = testDto;
-      return this.httpService
-              .post(`${this.VIDEO_SERVICE}/video/path`, { userId, nickName, videoPath, category })
-              .pipe( map( response => response.data ))
-    }
-    catch (err) {
-      console.log(err)
-      throw new HttpException(
-        '죄송해요 비디오 서버에 문제가 생겨 복구중이에요... router -> test/video/path',
-        HttpStatus.INTERNAL_SERVER_ERROR
-      )
-    }
+  testVideoPathRouter(@Body() testDto: any): Observable<AxiosResponse> {
+    const { userId, nickName, videoPath, category } = testDto;
+    return this.httpService
+      .post(`${this.VIDEO_SERVICE}/video/path`, { userId, nickName, videoPath, category })
+      .pipe(map(response => response.data))
   }
 
   /* 
@@ -178,19 +109,10 @@ export class ApiGateway {
   */
   @Put('like')
   pushLikeRequest(@Body() pushLikeDto: PushLikeDto): Observable<AxiosResponse> {
-    try {
-      const { userId, videoId } = pushLikeDto;
-      return this.httpService
-              .put(`${this.LIKE_SERVICE}/like`, { userId, videoId })
-              .pipe( map( response => response.data ))
-    }
-    catch (err) {
-      console.log(err)
-      throw new HttpException(
-        '죄송해요. 좋아요서버에 문제가 생겨 복구중이에요... router -> like',
-        HttpStatus.INTERNAL_SERVER_ERROR
-      );
-    }
+    const { userId, videoId } = pushLikeDto;
+    return this.httpService
+      .put(`${this.LIKE_SERVICE}/like`, { userId, videoId })
+      .pipe(map(response => response.data))
   }
 
   /*
@@ -200,27 +122,9 @@ export class ApiGateway {
   */
   @Get('search/video')
   searchVideoRequest(@Query('keyword') keyword: string): Observable<AxiosResponse> {
-    try {
-      return this.httpService
-              .get(`${this.SEARCH_SERVICE}/search/video`, { params: { keyword } })
-              .pipe( map( response => response.data ))
-      // return {
-      //   statusCode: 200,
-      //   message: 'OK',
-      //   body: {
-      //     data: [
-      //       ...data
-      //     ]
-      //   }
-      // }
-    }
-    catch (err) {
-      console.log(err)
-      throw new HttpException(
-        '죄송해요 검색 서버에 문제가 생겨 복구중이에요.... router -> search/video',
-        HttpStatus.INTERNAL_SERVER_ERROR
-      )
-    }
+    return this.httpService
+      .get(`${this.SEARCH_SERVICE}/search/video`, { params: { keyword } })
+      .pipe(map(response => response.data))
   }
 
   /*
@@ -230,18 +134,9 @@ export class ApiGateway {
   */
   @Post('video/comment')
   saveComment(@Body() saveCommentDto: SaveCommentDto): Observable<AxiosResponse> {
-    try {
-      const { videoId, userId, nickName, content } = saveCommentDto;
-      return this.httpService
-              .post(`${this.COMMENT_SERVICE}/video/comment`, { videoId, userId, nickName, content })
-              .pipe( map( response => response.data ))
-    }
-    catch (err) {
-      console.log(err)
-      throw new HttpException(
-        '죄송해요 댓글 서버에 문제가 생겨 복구중이에요.... router -> video/comment',
-        HttpStatus.INTERNAL_SERVER_ERROR
-      )
-    }
+    const { videoId, userId, nickName, content } = saveCommentDto;
+    return this.httpService
+      .post(`${this.COMMENT_SERVICE}/video/comment`, { videoId, userId, nickName, content })
+      .pipe(map(response => response.data))
   }
 }
