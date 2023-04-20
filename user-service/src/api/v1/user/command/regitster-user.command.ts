2530e55fe1de2aebd1638fde7ab2d55074220f02
JOOYUNHAK <wndbsgkr@naver.com>
2023-04-20 15:35:27 +0900

    delete: user directory for move

--- user-service/src/api/v1/user/command/regitster-user.command.ts
+++
@@ -1,7 +0,0 @@
-export class RegisterUserCommand {
-    constructor(
-        readonly id: string,
-        readonly phoneNumber:string,
-        readonly nickName: string
-    ) {}
-}
--- user-service/src/api/v1/user/query/find-user-ph.query.ts
+++
@@ -1,6 +0,0 @@
-import { IQuery } from '@nestjs/cqrs';
-export class FindUserByPhoneNumberQuery implements IQuery {
-    constructor(
-        readonly phoneNumber: string
-    ) {}
-}
