de9562773a2d220f014a0178500dcdd6f166a363
JOOYUNHAK <wndbsgkr@naver.com>
2023-04-21 02:37:06 +0900

    modify: auth module 정리

--- user-service/src/auth/auth.controller.ts
+++ user-service/src/auth/auth.controller.ts
@@ -1,22 +1,21 @@
-import { Controller, Post, Body, Get } from "@nestjs/common";
+import { Controller, Post, Body, UseInterceptors } from "@nestjs/common";
 import { ApiTags, ApiBody, ApiResponse, ApiOperation } from '@nestjs/swagger';
 import { LoginResponseDto } from "./dto/login-response.dto";
 import { CommandBus, QueryBus } from '@nestjs/cqrs';
 import { FindUserByPhoneNumberQuery } from "../user/query/find-user-ph.query";
 import { GetUserInfoQuery } from "../user/query/get-user-info.query";
 import { LoginRequestDto } from "./dto/login-requestdto";
-import { User } from "../user/entity/user.entity";
 import { RegisterUserCommand } from "../user/command/regitster-user.command";
-import { createResponse } from "../api/v1/generic/create-response";
-import { HttpService } from "@nestjs/axios";
+import { FindByPhoneNumberResult } from "src/user/dto/find-by-ph-result.dto";
+import { ResponseTransformInterceptor } from "src/interceptor/transform.interceptor";
 
 @ApiTags('Auth')
 @Controller('auth')
+@UseInterceptors(ResponseTransformInterceptor)
 export class AuthController {
     constructor(
         private queryBus: QueryBus,
         private commandBus: CommandBus,
-        private httpService: HttpService
     ) { }
     @ApiOperation({
         summary: '로그인',
@@ -38,49 +37,20 @@ export class AuthController {
     })
     @Post('login')
     async login(@Body() loginRequestDto: LoginRequestDto): Promise<LoginResponseDto> {
-        const { phoneNumber, nickName } = loginRequestDto;
-        const result = await this.queryBus.execute(new FindUserByPhoneNumberQuery(phoneNumber));
+
+        const findResult: FindByPhoneNumberResult =
+                await this.queryBus.execute(new FindUserByPhoneNumberQuery(loginRequestDto.phoneNumber))
         
         /* 로그인 성공 */
-        if (result) {
-            const { id } = result;
-            const [ {data}, userInfo ] = await Promise.all([
-                /* 좋아요 service에 해당 user의 좋아요 list 요청 */
-                this.httpService.axiosRef.get(`http://172.18.10.7:8082/like/list/${id}`),
-                this.queryBus.execute(new GetUserInfoQuery(id))
-            ]);
-            return {
-                statusCode: 200,
-                message: 'OK 기존 회원 로그인',
-                body: {
-                    user: {
-                        ...userInfo[0],
-                        likeList: [...data]
-                    }
-                }
-            };
-        }
-        return this.register(phoneNumber, nickName);
+        if( findResult )
+            return await this.queryBus.execute( new GetUserInfoQuery( findResult.id ));     
+        return this.register(loginRequestDto);
     }
 
-    async register(phoneNumber:string, nickName: string): Promise<LoginResponseDto> {
+    async register(loginRequestDto: LoginRequestDto): Promise<LoginResponseDto> {
         /* 회원가입 */
-        const uuid = User.createOrderedUuid();
-        await this.commandBus.execute(new RegisterUserCommand(uuid, phoneNumber, nickName));
-        const [ {data}, userInfo ] = await Promise.all([
-            /* 좋아요 service에 해당 user의 좋아요 list 요청 */
-            this.httpService.axiosRef.get(`http://172.18.10.7:8082/like/list/${uuid}`),
-            this.queryBus.execute(new GetUserInfoQuery(uuid))
-        ]);
-        return {
-            statusCode: 200,
-            message: 'OK 새로운 회원 로그인',
-            body: {
-                user: {
-                    ...userInfo[0],
-                    likeList: [...data]
-                }
-            }
-        };
+        const { phoneNumber, nickName } = loginRequestDto;
+        await this.commandBus.execute(new RegisterUserCommand( phoneNumber, nickName ));        
+        return await this.queryBus.execute(new GetUserInfoQuery(phoneNumber));
     }
 }
