e38af4ac6a7a1b3fbcbd2e02a5598c2d1f3ddae0
JOOYUNHAK <wndbsgkr@naver.com>
2023-05-07 05:13:42 +0900

    add(video): summary result event handler
    summary result, video delete handler

---
+++ video-service/src/video/application/event/handler/summary-success-event.handler.ts
@@ -0,0 +1,14 @@
+import { EventsHandler, IEventHandler } from "@nestjs/cqrs";
+import { SummarySuccessEvent } from "src/video/domain/video/event/summary-success.event";
+import { VideoRepository } from "src/video/infra/database/video.repository";
+
+@EventsHandler(SummarySuccessEvent)
+export class SummarySuccessEventHandler implements IEventHandler<SummarySuccessEvent> {
+    constructor(
+        private readonly videoRepository: VideoRepository
+    ) {}
+
+    async handle(event: SummarySuccessEvent) {
+        await this.videoRepository.updateSummaryInfo(event.video);
+    }
+}
