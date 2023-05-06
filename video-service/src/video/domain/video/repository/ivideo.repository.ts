966eb56613eee49a36db1b8d42546a54558e94a7
JOOYUNHAK <wndbsgkr@naver.com>
2023-05-07 05:08:06 +0900

    add(video): video repository

--- video-service/src/video/domain/video/repository/ivideo.repository.ts
+++ video-service/src/video/domain/video/repository/ivideo.repository.ts
@@ -1,6 +1,9 @@
+import { VideoEntity } from "../entity/video.entity";
 import { Video } from "../video-domain";
 
 export interface IVideoRepository {
+    save: (videoEntity: VideoEntity) => Promise<void>;
     updateSummaryInfo: (video: Video) => Promise<void>;
     findSummaryInfoById: (videoId: string) => Promise<Video>;
+    deleteSummaryInfoById: (videoId: string) => Promise<void>;
 }
