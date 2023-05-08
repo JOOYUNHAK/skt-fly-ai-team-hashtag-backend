c649921b0742639c554d3c3d28b8a9af2e4399ab
JOOYUNHAK <wndbsgkr@naver.com>
2023-05-09 05:08:19 +0900

    modify(video): delete video event name
    행위를 지시하는 이벤트 이름 변경

--- video-service/src/video/application/event/video-deleted.event.ts
+++
@@ -1,6 +0,0 @@
-import { IEvent } from "@nestjs/cqrs";
-import { Video } from "src/video/domain/video/video-domain";
-
-export class VideoDeletedEvent implements IEvent {
-    constructor( readonly video: Video ) {}
-}
