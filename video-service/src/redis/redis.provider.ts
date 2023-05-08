c554c67bcef21cdf09b938579162990c3797f5f5
JOOYUNHAK <wndbsgkr@naver.com>
2023-05-09 00:02:43 +0900

    modify(redis): url to config file

--- video-service/src/redis/redis.provider.ts
+++ video-service/src/redis/redis.provider.ts
@@ -1,11 +1,14 @@
+import { ConfigModule, ConfigService } from '@nestjs/config';
 import { createClient } from 'redis';
 
 export const redisProvider = [
     {
         provide: 'REDIS_CLIENT',
-        useFactory: async () => {
+        import: [ConfigModule],
+        inject: [ConfigService],
+        useFactory: async (configService: ConfigService) => {
             const client = createClient({
-                url: 'redis://172.18.10.11:6379'
+                url: configService.get('redis.url')
             });
             await client.connect();
             return client;
