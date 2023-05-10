c0fc3b132352121d015f8c52161fbcce6fcd3ff6
JOOYUNHAK <wndbsgkr@naver.com>
2023-05-11 05:34:50 +0900

    refactor(comment): comment add logic move to video
    댓글 입력 로직 비디오 서비스로 이동

--- comment-service/src/comment.module.ts
+++ comment-service/src/comment.module.ts
@@ -4,7 +4,6 @@ import { ConfigModule } from "@nestjs/config";
 import { CqrsModule } from '@nestjs/cqrs';
 import databaseConfiguration from 'config/database.configuration';
 import { DatabaseModule } from 'database/database.module';
-import { SaveCommentCommandHandler } from './command/handler/save-comment-command.handler';
 import { CommentController } from './comment.controller';
 import { GetVideoCommentsQueryHandler } from './query/handler/get-video-comments-query.handler';
 
@@ -19,7 +18,6 @@ import { GetVideoCommentsQueryHandler } from './query/handler/get-video-comments
   ],
   controllers: [CommentController],
   providers: [
-    SaveCommentCommandHandler,
     GetVideoCommentsQueryHandler
   ]
 })
