a92fc767d3d58dcd52fe0b52e1b2d2ba1aef0421
JOOYUNHAK <wndbsgkr@naver.com>
2023-05-15 04:17:08 +0900

    fix: untracked files

--- api-gateway/config/api-gateway.configuration.ts
+++
@@ -1,9 +0,0 @@
-export default () => ({
-    docker: {
-        user_service: 'http://172.18.10.5:8080',
-        video_service: 'http://172.18.10.6:8081',
-        like_service: 'http://172.18.10.7:8082',
-        search_service: 'http://172.18.10.8:8083',
-        comment_service: 'http://172.18.10.9:8084'
-    }
-})
