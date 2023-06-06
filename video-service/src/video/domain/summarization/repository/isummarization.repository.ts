55428ada8df4c1a2fae991a3f4b6e8e076724d02
JOOYUNHAK <wndbsgkr@naver.com>
2023-06-06 17:20:54 +0900

    modify(repository): method to typeorm api
    summarization repository

--- video-service/src/video/domain/summarization/repository/isummarization.repository.ts
+++ video-service/src/video/domain/summarization/repository/isummarization.repository.ts
@@ -1,7 +1,5 @@
 import { Summarization } from "../entity/summarization.entity";
 
 export interface ISummarizationRepository {
-    save: (summariztion: Summarization) => Promise<Summarization>;
-    delete:(id: Summarization) => Promise<void>;
     findById: (id: number) => Promise<Summarization>;
 }
